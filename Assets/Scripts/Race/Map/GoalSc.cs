25d99aed0fce3e819d54f485fceb5ada60233aa2
C24004-PC\c24004 <c24004@std.it-college.ac.jp>
2025-12-15 11:18:25 +0900

    バグ修正、調整

--- Assets/Scripts/Race/Map/GoalSc.cs
+++ Assets/Scripts/Race/Map/GoalSc.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalSc : MonoBehaviour
 {
     [SerializeField] RaceManager raceManager;
+    [SerializeField] TextMeshProUGUI[] lapText;
+    Dictionary<int, int> textDic = new();
+
+    private void Start()
+    {
+        for (int i = 0; i < raceManager.playerDatas.Count; i++)
+        {
+            lapText[i].text = 0 + "/" + raceManager.lapCount;
+            textDic.Add(raceManager.playerDatas[i].playerNum,i);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            PlayerData playerData = collision.transform.parent.GetComponent<PlayerManager>().playerData;
+            PlayerManager pm = collision.transform.parent.GetComponent<PlayerManager>();
+            
             //ƒS[ƒ‹
-            if (playerData.progress == 9)
+            if (pm.playerData.progress == 9)
             {
-                if (playerData.lapCount >= raceManager.lapCount)
+                if (pm.playerData.lapCount >= raceManager.lapCount)
                 {
                     raceManager.PlayFinishAnimation();
+                }
+                
+                if (pm.playerData.lapCount <= 0)
+                {
+                    lapText[pm.playerNum].text = 0 + "/" + raceManager.lapCount;
+                }
+                else
+                {
+                    lapText[textDic[pm.playerNum]].text = pm.playerData.lapCount + "/" + raceManager.lapCount;
+
                 }
             }
         }
